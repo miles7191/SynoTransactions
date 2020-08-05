13c6f3d2c8fd9fc41e4526e83b869fb1b3294e2c
Matthew Rosato <miles7191@gmail.com>
2020-08-05 15:10:45 -0700

    Added BOPParser Test

---
+++ src/test/java/mcd/BOPParserTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2020 Matthew Rosato
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package mcd;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.t07m.synotransactions.mcd.BOPParser;
+import com.t07m.synotransactions.mcd.MCDTransaction;
+
+public class BOPParserTest {
+
+	private static String bopDir = "src/test/resources/mcd/bops";
+
+	private static BOPParser bopParser;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		bopParser = new BOPParser();
+	}
+
+	@Test
+	public void testAllBOPS() {
+		File dir = new File(bopDir);
+		for(File file : dir.listFiles()) {
+			assert(testBOP(file));
+		}
+	}
+
+	public boolean testBOP(File file) {
+		try {
+			MCDTransaction trans = bopParser.parse(file);
+			if(trans != null) {
+				if(trans.getKS() == -1) {
+					System.out.println("KS Parse Failed: " + file.getName());
+					return false;
+				}
+				return true;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("General Parse Failed: " + file.getName());
+		return false;
+	}
+
+}
