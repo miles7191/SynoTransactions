0cf5801e1c9f5a376abc042904c471a41d7e2034
Matthew Rosato <miles7191@gmail.com>
2020-08-14 13:29:47 -0700

    Added KeyStation Class

---
+++ src/main/java/com/t07m/synotransactions/mcd/KeyStation.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2020 Matthew Rosato
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.t07m.synotransactions.mcd;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeyStation {
+
+	private @Getter @Setter(AccessLevel.PACKAGE) int ID;
+	private @Getter @Setter(AccessLevel.PACKAGE) boolean enabled;
+	private @Getter @Setter(AccessLevel.PACKAGE) String IP;
+	private @Getter @Setter(AccessLevel.PACKAGE) String domain;
+	private @Getter @Setter(AccessLevel.PACKAGE) String username;
+	private @Getter @Setter(AccessLevel.PACKAGE) String password;
+	private @Getter @Setter(AccessLevel.PACKAGE) String reprintPath;
+	private @Getter @Setter(AccessLevel.PACKAGE) String SynologyDeviceName;
+	
+}
